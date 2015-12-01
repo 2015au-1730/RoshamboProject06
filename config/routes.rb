cb3c5a0df3441a8fdb26832b0d8bbecc94a2b73d
maggiezhang0712 <maggie.zhang.0712@gmail.com>
2015-12-01 18:06:39 -0500

    Merge branch 'master' of https://github.com/2015au-1730/RoshamboProject06
    # Conflicts:
    #	.idea/RoshamboProject06.iml
    #	.idea/runConfigurations/Development__RoshamboProject06.xml
    #	.idea/runConfigurations/Production__RoshamboProject06.xml
    #	.idea/runConfigurations/spec__RoshamboProject06.xml
    #	.idea/runConfigurations/test__RoshamboProject06.xml

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   get 'tool_kit' => 'tool_kit#index'
+  get 'list' => 'pages#index'
+  get 'forum'=>'posts#index'
   root 'pages#home'
 end
