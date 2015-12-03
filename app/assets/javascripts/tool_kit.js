d86f30710c1ca6473a90218a54b5ba6814ae0e85
annabaker <anna.baker.313@gmail.com>
2015-12-03 14:45:42 -0500

    Update tool_kit.js

--- app/assets/javascripts/tool_kit.js
+++ app/assets/javascripts/tool_kit.js
@@ -5,13 +5,13 @@
             var finalBmi = weight/(height/100*height/100)
             document.bmiForm.bmi.value = finalBmi
             if(finalBmi < 18.5){
-                document.bmiForm.meaning.value = "That you are too thin."
+                document.bmiForm.meaning.value = "you are too thin."
             }
             if(finalBmi > 18.5 && finalBmi < 25){
-                document.bmiForm.meaning.value = "That you are healthy."
+                document.bmiForm.meaning.value = "you are healthy."
             }
             if(finalBmi > 25){
-                document.bmiForm.meaning.value = "That you are overweight."
+                document.bmiForm.meaning.value = "you are overweight."
             }
         }
         else{
